10e7be35c30e2f0c9ed8510e5c0d8731c425307c
patrickBakin <68882850+patrickBakin@users.noreply.github.com>
2022-03-05 15:03:47 +0700

    supporting utf-8

--- DiscordLink.py
+++ DiscordLink.py
@@ -2,9 +2,10 @@ import socket
 import time
 
 from discord import Webhook, RequestsWebhookAdapter # Importing discord.Webhook and discord.RequestsWebhookAdapter
+import unicodedata2
 
-webhook = Webhook.from_url('Your Discord Webhook Url', adapter=RequestsWebhookAdapter()) # Initializing webhook
-webhook.send(content="Hello World")
+webhook = Webhook.from_url('https://discordapp.com/api/webhooks/949141290971455588/F7RJDbC4svqUIVSutORjQseoXhJJmWRUjqHKRqXVoPGc_Uf7M25RnftCVWxLXdYGBpFT', adapter=RequestsWebhookAdapter()) # Initializing webhook
+
 
 def Connect():
     global client
@@ -28,14 +29,21 @@ def Connect():
 def HandleMessage():
     while True:
 
-        data = client.recv(1024).decode()
-        if (data == ""):
+        data = client.recv(1024)
+        if (data == bytes()):
             client.close()
 
             Connect()
             break
         else:
-            webhook.send(data)
 
+           webhook.send(decodedata(data.decode('utf-8')))
+
+def decodedata(string):
+    numarray=string.split('/')
+    Text=""
+    for numi in range(len(numarray)):
+        Text=Text+chr(int(numarray[numi]))
+    return Text
 
 Connect()
